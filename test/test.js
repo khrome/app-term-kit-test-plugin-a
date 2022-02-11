3cd07d6042b93f92006a69a61cc038667e060422
Abbey Sparrow <abbey@khrome.net>
2022-02-10 19:43:16 -0700

    first commit

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const should = require('chai').should();
+
+describe('test-plugin', ()=>{
+    it('loads', (done)=>{
+        let a = require('../app-term-kit-test-plugin-a');
+        done();
+    })
+});
